73c29ed9d3ab18dac5b623d971728667d6b5dd45
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-06-07 13:37:20 -0300

    serializers, settigs

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Movie
+from .serializers import MovieSerializer
 
-# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = {
+        "vote_average": ["gte"],
+        "release_date": ["gte", "lte"],
+    }
+    search_fields = ["title", "overview"]
+    ordering_fields = ["title", "popularity", "release_date"]
